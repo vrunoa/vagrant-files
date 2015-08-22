c484dbd7d8121d41bcbf6c6437f963cb803fd96b
Bruno Alassia <vruno.alassia@gmail.com>
2015-08-22 16:53:38 -0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install curl
 sudo apt-get install git
-sudo apt.get install make
+sudo apt-get install make
 sudo apt-get install vim
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
 nvm install 0.11
